1e56a3ec1eeb5fa5540d84feca3315813022f8aa
whitneydawson123 <whitneydawson123@gmail.com>
2022-05-04 19:14:59 -0400

    finished

--- assests/js/script.js
+++ assests/js/script.js
@@ -45,6 +45,24 @@ async function getMoreSongs(url) {
   showData(data);
 }
 
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  if (data.error) {
+    result.innerHTML = data.error;
+  } else {
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`;
+
+  }
+  
+  more.innerHTML = '';
+}
+
 // Event listeners
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -57,3 +75,15 @@ form.addEventListener('submit', e => {
     searchSongs(searchTerm);
   }
 })
+
+// Get lyrics button click
+result.addEventListener('click', e => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
+  }
+});
