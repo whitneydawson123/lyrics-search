4d9013aa168b1a843149ab7366d8619739739329
whitneydawson123 <whitneydawson123@gmail.com>
2022-05-04 18:57:08 -0400

    updated

--- assests/js/script.js
+++ assests/js/script.js
@@ -15,7 +15,34 @@ async function searchSongs(term) {
 
 // Show song and artist in DOM
 function showData(data) {
-  
+  result.innerHTML = `
+    <ul class="songs">
+      ${data.data.map(song =>
+    `<li>
+          <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+          <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+          </li>
+        `
+  ).join('')}
+  </ul>
+  `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ''}
+      ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}
+    `;
+  } else {
+    more.innerHTML = '';
+  }
+}
+
+// Get prev and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
 }
 
 // Event listeners
